811d9d03688f9de2089cb9c466076e1808d02ce6
Mihail Tudos <mihailtudos@outlook.com>
2023-11-12 13:03:50 +0200

    documented the products Handler

--- handlers/products.go
+++ handlers/products.go
@@ -8,14 +8,17 @@ import (
 	"github.com/mihailtudos/microservices/data"
 )
 
+// Products is a http.Handler
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts creates a products handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the Handler and satisfies the http.Handler interface
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		p.getProducts(w, r)
@@ -28,16 +31,24 @@ func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+// getProducts returns the products from the data store
 func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
 	lp := data.GetProducts()
+
+	// set content-type header
 	w.Header().Add("Content-Type", "application/json")
 
+	// serialize the products list to JSON
 	err := lp.ToJSON(w)
 	if err != nil {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 	}
 }
 
+// updateProduct updates a product to the received body
 func (p *Products) updateProduct(w http.ResponseWriter, r *http.Request) {
 	p.l.Printf("\n\n%#v\n\n", strings.Split(r.URL.Path, "/products/"))
 }
